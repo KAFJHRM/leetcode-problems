dfa2f8da0ce1c18eeff2122cb486414481beae31
KAFJHRM <afzalkiller13579@gmail.com>
2026-07-17 13:59:47 +0530

    Refactor findNumbers method for Java syntax

--- Day1 HW-11295 Find Numbers With Even Number of Digits.java
+++ Day1 HW-11295 Find Numbers With Even Number of Digits.java
@@ -1,17 +1,11 @@
 class Solution {
-public:
-    int findNumbers(vector<int>& nums) {
+    public int findNumbers(int[] nums) {
         int count = 0;
         for (int num : nums) {
-            // Under constraints (1 <= num <= 10^5), a number has an even number of digits if:
-            // - It has 2 digits: [10, 99]
-            // - It has 4 digits: [1000, 9999]
-            // - It has 6 digits: 100000 (though upper constraint is 10^5, which is 6 digits)
             if ((num >= 10 && num <= 99) || (num >= 1000 && num <= 9999) || num == 100000) {
                 count++;
             }
         }
         return count;
-        
     }
-};
+}

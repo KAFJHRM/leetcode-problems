2337c560022c5604fff7bd0735c5c5c3a109a2ee
KAFJHRM <afzalkiller13579@gmail.com>
2026-07-23 11:25:55 +0530

    Create Day 9-1812 Determine color  of a chessboard square.java

---
+++ Day 9-1812 Determine color  of a chessboard square.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        char col = coordinates.charAt(0);
+        char row = coordinates.charAt(1);
+        
+        return (col + row) % 2 != 0;
+    }
+}
+
